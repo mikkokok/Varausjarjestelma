11fc867f2624ffb2c045198413c04ca639636073
Vahtix <vahtis91@gmail.com>
2017-02-18 19:22:07 +0200

    Login formia muokattu ja ylläpidon runko tehty

--- Varausjarjestelma/Login.xaml.cs
+++ Varausjarjestelma/Login.xaml.cs
@@ -19,19 +19,65 @@ namespace Varausjarjestelma
     /// </summary>
     public partial class Login : Window
     {
+
+        private readonly int mainWidth = 800;
+        private readonly int mainHeight = 600;
+
+        private String username;
+        private String password;
+
         public Login()
         {
             InitializeComponent();
+            Application.Current.MainWindow = this;
         }
 
         private void btnkirjaudu_Click(object sender, RoutedEventArgs e)
         {
+            this.username = txt_kayttajaNimi.Text;
+            this.password = txt_salasana.Text;
 
+            if (this.username == "Matti" && this.password == "Matti")
+            {
+                lbl_ilmoitus.Content = "Kirjautuminen onnistui";
+                lbl_ilmoitus.Foreground = new SolidColorBrush(Colors.White);
+                Login_Grid.Visibility = Visibility.Collapsed;
+                Application.Current.MainWindow.Width = this.mainWidth;
+                Application.Current.MainWindow.Height = this.mainHeight;
+                YllapidonControl.Visibility = Visibility.Visible;
+
+            }
+            else
+            {
+                lbl_ilmoitus.Visibility = Visibility.Visible;
+                lbl_ilmoitus.Content = "Käyttäjänimi tai salasana on väärin";
+                lbl_ilmoitus.Foreground = new SolidColorBrush(Colors.Red);
+            }
         }
 
         private void btn_rekisteroidy_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key== Key.Enter)
+            {
+                btnkirjaudu_Click(sender, e);
+            }
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (e.PreviousSize == e.NewSize)
+                return;
+
+            var w = SystemParameters.PrimaryScreenWidth;
+            var h = SystemParameters.PrimaryScreenHeight;
+
+            this.Left = (w - e.NewSize.Width) / 2;
+            this.Top = (h - e.NewSize.Height) / 2;
         }
     }
 }
