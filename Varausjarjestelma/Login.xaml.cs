a820ce4e9fadc6507960c708dce5624206c9e19d
Vahtix <vahtis91@gmail.com>
2017-02-18 22:19:36 +0200

    Login Formia muokattu Oikea käyttäjänimi ja salasana on Matti

--- Varausjarjestelma/Login.xaml.cs
+++ Varausjarjestelma/Login.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,12 +20,15 @@ namespace Varausjarjestelma
     /// </summary>
     public partial class Login : Window
     {
-
+        //Pääikkunan koko
         private readonly int mainWidth = 800;
         private readonly int mainHeight = 600;
 
+        //Annetut käyttäjänimi ja salasana
         private String username;
         private String password;
+
+        private int rooli = 1; // 0 = asiakas 1 = ylläpitäjä
 
         public Login()
         {
@@ -34,23 +38,44 @@ namespace Varausjarjestelma
 
         private void btnkirjaudu_Click(object sender, RoutedEventArgs e)
         {
+            //Alustetaan muuttujat tekstilaatikoiden avulla
             this.username = txt_kayttajaNimi.Text;
             this.password = txt_salasana.Text;
 
+            //Toiminnot jos käyttäjänimi ja salasana ovat oikein
             if (this.username == "Matti" && this.password == "Matti")
             {
-                lbl_ilmoitus.Content = "Kirjautuminen onnistui";
+                //Tulostetaan ilmoitus käyttäjälle
                 lbl_ilmoitus.Foreground = new SolidColorBrush(Colors.White);
+                lbl_ilmoitus.Content = "Kirjautuminen onnistui";
+                lbl_ilmoitus.Visibility = Visibility.Visible;
+
+                //Thread.Sleep(1000);
+
+                //Piilotetaan Login ja muutetaan ikkunan kokoa
                 Login_Grid.Visibility = Visibility.Collapsed;
                 Application.Current.MainWindow.Width = this.mainWidth;
                 Application.Current.MainWindow.Height = this.mainHeight;
-                YllapidonControl.Visibility = Visibility.Visible;
+
+                //Käyttäjän roolin mukaan avataan käyttäjälle tarkoitettu näkymä
+                if (this.rooli == 1)
+                {
+                    this.Title = "Varausjärjestelmän Ylläpito";
+                    YllapidonControl.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    this.Title = "Elokuvalippujen Varausjärjestelmä";
+                    AsiakkaanControl.Visibility = Visibility.Visible;
+                }
+                
 
             }
+            //Virheilmoitus jos käyttäjänimi/salasana ovat väärin
             else
             {
                 lbl_ilmoitus.Visibility = Visibility.Visible;
-                lbl_ilmoitus.Content = "Käyttäjänimi tai salasana on väärin";
+                lbl_ilmoitus.Content = "Väärä käyttäjänimi tai salasana";
                 lbl_ilmoitus.Foreground = new SolidColorBrush(Colors.Red);
             }
         }
@@ -60,24 +85,27 @@ namespace Varausjarjestelma
 
         }
 
+        //Toiminnot Enter-painikkeelle
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.Key== Key.Enter)
+            //Jos käyttäjä on login formissa ja painaa enteriä niin yritetään kirjautua sisään
+            if (e.Key== Key.Enter && Login_Grid.Visibility == Visibility.Visible)
             {
                 btnkirjaudu_Click(sender, e);
             }
         }
 
+        //Metodi joka sijoittaa ikkunan keskelle tietokoneen näyttöä kun ikkunan koko muuttuu
         private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
         {
             if (e.PreviousSize == e.NewSize)
                 return;
 
-            var w = SystemParameters.PrimaryScreenWidth;
-            var h = SystemParameters.PrimaryScreenHeight;
+            var width = SystemParameters.PrimaryScreenWidth;
+            var height = SystemParameters.PrimaryScreenHeight;
 
-            this.Left = (w - e.NewSize.Width) / 2;
-            this.Top = (h - e.NewSize.Height) / 2;
+            this.Left = (width - e.NewSize.Width) / 2;
+            this.Top = (height - e.NewSize.Height) / 2;
         }
     }
 }
